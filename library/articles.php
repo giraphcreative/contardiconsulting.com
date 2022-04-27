393f009634da5d8a4d878f5f9f607ad921da4448
James Pederson <james@jpederson.com>
2022-04-27 15:17:09 -0500

    Add location field to posts, sorting options to articles shortcode, and new 'projects' style for articles.

--- library/articles.php
+++ library/articles.php
@@ -9,7 +9,9 @@ function articles_shortcode( $atts ) {
 		'cats' => '',
 		'category__not_in' => '',
 		'post_type' => 'post',
-		'posts_per_page' => 3
+		'posts_per_page' => 3,
+		'orderby' => 'date',
+		'order' => 'DESC'
 	), $atts );
 
 	$args = array(
@@ -28,34 +30,64 @@ function articles_shortcode( $atts ) {
 		$args['category__not_in'] = explode( ',', $a['category__not_in'] );
 	}
 
+	$args['order'] = $a['order'];
+	$args['orderby'] = $a['orderby'];
+
 	$query = new WP_Query( $args );
 
 	// Check that we have query results.
 	if ( $query->have_posts() ) {
 
-		$return = '<div class="article-cards">';
-	  
-	    // Start looping over the query results.
-	    while ( $query->have_posts() ) {
-	        $query->the_post();
-	        $categories = get_the_category();
-	        $cat = $categories[0];
-	        $return .= '<div class="entry">';
-	        $return .= '<div class="entry-thumbnail">';
-	        $return .= '<a href="' . get_the_permalink() . '">';
-	        $return .= get_the_post_thumbnail( null, array( 768, 480 ) );
-	        $return .= '</a>';
-		    $return .= '<div class="entry-category ' . $cat->slug . '">' . $cat->name . '</div>';
-		    $return .= '</div>';
-	        $return .= '<div class="entry-inner">';
-		    $return .= '<h4><a href="' . get_the_permalink() . '">' . get_the_title() . '</a></h4>';
-		    $return .= wpautop( get_the_excerpt() );
-		    $return .= '</div>';
-		    $return .= '</div>';
-	    }
+		if ( $a['style'] == 'projects' ) {
 
-		$return .= '</div>';
-	  
+			$return = '<div class="article-cards">';
+		  
+		    // Start looping over the query results.
+		    while ( $query->have_posts() ) {
+		        $query->the_post();
+		        $location = get_cmb_value('location');
+		        $return .= '<div class="entry">';
+		        $return .= '<div class="entry-thumbnail">';
+		        $return .= '<a href="' . get_the_permalink() . '">';
+		        $return .= get_the_post_thumbnail( null, array( 768, 480 ) );
+		        $return .= '</a>';
+			    if ( !empty( $location ) ) $return .= '<div class="entry-category">' . $location . '</div>';
+			    $return .= '</div>';
+		        $return .= '<div class="entry-inner">';
+			    $return .= '<h4><a href="' . get_the_permalink() . '">' . get_the_title() . '</a></h4>';
+			    $return .= '</div>';
+			    $return .= '</div>';
+		    }
+
+			$return .= '</div>';
+			
+		} else {
+
+			$return = '<div class="article-cards">';
+		  
+		    // Start looping over the query results.
+		    while ( $query->have_posts() ) {
+		        $query->the_post();
+		        $categories = get_the_category();
+		        $cat = $categories[0];
+		        $return .= '<div class="entry">';
+		        $return .= '<div class="entry-thumbnail">';
+		        $return .= '<a href="' . get_the_permalink() . '">';
+		        $return .= get_the_post_thumbnail( null, array( 768, 480 ) );
+		        $return .= '</a>';
+			    $return .= '<div class="entry-category ' . $cat->slug . '">' . $cat->name . '</div>';
+			    $return .= '</div>';
+		        $return .= '<div class="entry-inner">';
+			    $return .= '<h4><a href="' . get_the_permalink() . '">' . get_the_title() . '</a></h4>';
+			    $return .= wpautop( get_the_excerpt() );
+			    $return .= '</div>';
+			    $return .= '</div>';
+		    }
+
+			$return .= '</div>';
+			
+		}
+
 	} else {
 		return '';
 	}
@@ -68,5 +100,28 @@ function articles_shortcode( $atts ) {
 	return $return;
 }
 add_shortcode( 'articles', 'articles_shortcode' );
+
+
+// add the title metabox
+function article_metabox( $meta_boxes ) {
+
+    $article_metabox = new_cmb2_box( array(
+        'id' => 'article_metabox',
+        'title' => 'Articles',
+        'object_types' => array( 'post' ), // post type
+        'context' => 'normal',
+        'priority' => 'high',
+    ) );
+
+    $article_metabox->add_field( array(
+        'name' => 'Location',
+        'id'   => 'location',
+        'type' => 'text',
+    ) );
+
+}
+add_filter( 'cmb2_init', 'article_metabox' );
+
+
 
 
