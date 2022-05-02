a0ab4be3d2a470851a118fd70609be1df0341d6e
James Pederson <james@jpederson.com>
2022-05-02 10:04:02 -0500

    Add three projects to the homepage.

--- page-front.php
+++ page-front.php
@@ -26,38 +26,10 @@ get_header();
 		<a href="/process"><?php the_post_thumbnail( 'full' ); ?></a>
 	</div>
 
-	<!--
-	<div class="articles">
-	<?php
-	/*
-	$args = array(
-		'posts_per_page' => 3
-	);
-	
-	// query
-	$the_query = new WP_Query( $args );
-
-	// loop
-	if ( $the_query->have_posts() ) {
-	    while ( $the_query->have_posts() ) {
-	        $the_query->the_post(); ?>
-	        <div class="article">
-	        	<a href="<?php the_permalink() ?>"><?php the_post_thumbnail(); ?></a>
-	        	<h3><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h3>
-	        	<?php the_excerpt(); ?>
-	        	<p class="read-more"><a href="<?php the_permalink(); ?>">Read more &raquo;</a></p>
-	        </div>
-	        <?php
-	    }
-	}
-	
-	// reset post data
-	wp_reset_postdata();
-	
-	*/
-	?>
+	<div class="content-wide">
+		<h3><strong>CONTARDI CONSULTING PROJECTS</strong></h3>
+		<?php print do_shortcode( '[articles cats="projects" style="projects" orderby="title" order="ASC" posts_per_page=3 /]' ) ?>
 	</div>
-	-->
 
 <?php
 
